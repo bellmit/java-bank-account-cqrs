ddfcf7b31cb196b0b87e677bd263fd7a5986bda9
Felipe Possari <fcpfelipe@hotmail.com>
2022-02-14 15:42:39 -0300

    Create events objects and setup dependency

---
+++ bank-account/account.common/src/main/java/com/felipepossari/bank/account/common/events/FundsDepositedEvent.java
@@ -0,0 +1,15 @@
+package com.felipepossari.bank.account.common.events;
+
+import com.felipepossari.bank.cqrs.core.events.BaseEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class FundsDepositedEvent extends BaseEvent {
+    private double amount;
+}
