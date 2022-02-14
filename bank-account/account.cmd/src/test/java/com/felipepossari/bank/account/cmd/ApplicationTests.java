ea281a7b56e733328823fc3f134082469a0b6bab
Felipe Possari <fcpfelipe@hotmail.com>
2022-02-14 14:52:36 -0300

    Initial commit

---
+++ bank-account/account.cmd/src/test/java/com/felipepossari/bank/account/cmd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipepossari.bank.account.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
